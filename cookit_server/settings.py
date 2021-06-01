5a0f90d04183dfd73d739d8fdc0602b16d76858b
Heath Lester <h.allen.lester@gmail.com>
2021-06-01 10:54:56 -0500

    edited cors origin whitelist

--- cookit_server/settings.py
+++ cookit_server/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://django-env.eba-ssefm7mw.us-east-1.elasticbeanstalk.com'
+    'https://cookit-react.herokuapp.com'
 )
 
 MIDDLEWARE = [
