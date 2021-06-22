7be4635811849108c16b1ac28af0684ca3360701
Heath Lester <h.allen.lester@gmail.com>
2021-06-22 14:00:55 -0500

    added whitelist

--- cookit_server/settings.py
+++ cookit_server/settings.py
@@ -70,7 +70,9 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://cookit-react.herokuapp.com'
+    'https://cookit-react.herokuapp.com',
+    '172.31.88.105',
+    '172.31.30.94'
 )
 
 MIDDLEWARE = [
